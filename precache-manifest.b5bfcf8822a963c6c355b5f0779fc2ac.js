4c2118d7d6e6869fe8e4621029984923cc077e91
רחל בודנשטיין <u61499@sii.org.il>
2020-10-26 21:06:29 +0200

    Updates

---
+++ precache-manifest.b5bfcf8822a963c6c355b5f0779fc2ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39d1d719992c297e99ea8bf9d5818c6",
+    "url": "/face-ai/index.html"
+  },
+  {
+    "revision": "5c50bbe5b9daa90fc400",
+    "url": "/face-ai/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ccd2f12324cdaa66fa0d",
+    "url": "/face-ai/static/css/main.1719710f.chunk.css"
+  },
+  {
+    "revision": "5c50bbe5b9daa90fc400",
+    "url": "/face-ai/static/js/2.9d30c3b0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-ai/static/js/2.9d30c3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd2f12324cdaa66fa0d",
+    "url": "/face-ai/static/js/main.28d5920f.chunk.js"
+  },
+  {
+    "revision": "57fa2ec60f4c77dc25ce",
+    "url": "/face-ai/static/js/runtime-main.da57d613.js"
+  }
+]);

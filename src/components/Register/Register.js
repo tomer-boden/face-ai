2a5fbfc259344a7dfaa4302bb7ed98a99869acc3
tomer.b <tomer.bod@gmail.com>
2021-04-14 13:30:57 +0300

    test

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -76,7 +76,7 @@ const Register = ({onRouteChange, loadUser}) => {
       <input 
       className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
       type="submit" 
-      value="Sign in" 
+      value="Sign up" 
       onClick={onSumbit}/>
     </div>
   </div>
